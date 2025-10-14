05cf896e40768fc6f7b20065632e54edd565d2d6
anghel3m <adms95@outlook.es>
2025-10-14 15:18:08 -0500

    conexion

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -11,18 +10,18 @@ MYSQL_PORT = os.getenv("MYSQL_PORT")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_SSL_CA = os.getenv("MYSQL_SSL_CA")
 
 DATABASE_URL = (
     f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {"ca": MYSQL_SSL_CA}
+    }
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()

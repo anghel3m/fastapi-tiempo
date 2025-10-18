d3136a16c6b3c231a4e4dd6e10754eb191a19002
anghel3m <adms95@outlook.es>
2025-10-18 09:30:21 -0500

    main localhost

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import tiempo
 
 app = FastAPI(title="API FastAPI Tiempo")
 
+# 🔹 Configurar CORS
+origins = [
+    "http://localhost:4200",          # Angular en desarrollo
+    "https://penalista-tools.netlify.app",  # tu dominio en producción (ajústalo si cambia)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # puedes usar ["*"] si quieres permitir todo
+    allow_credentials=True,
+    allow_methods=["*"],          # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],          # Permitir todos los headers
+)
+
+# 🔹 Registrar routers
 app.include_router(tiempo.router)
 
 @app.get("/")
